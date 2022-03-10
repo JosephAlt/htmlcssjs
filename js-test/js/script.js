fac5dfa1bd372262dd94ddd62f2b7714278b14fd
Joseph Alt <josephsonyalt@gmail.com>
2022-03-10 14:33:02 +0530

    data types in js

--- js-test/js/script.js
+++ js-test/js/script.js
@@ -10,3 +10,16 @@ function b (){
     console.log("b: message = " + message);
 }
 a();
+
+//data types
+var boolval=true;
+var nullval=null;
+var undefinedval;
+var numval= 8;
+var stringval= "noice";
+console.log(boolval);
+console.log(nullval);
+console.log(undefinedval);
+console.log(stringval);
+console.log(numval);
+
